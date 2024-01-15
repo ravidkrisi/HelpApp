27754471afea3a8aa31e6f1c50b8db9538d39682
ravidkris <ravidkrisi22@gmail.com>
2024-01-15 13:19:57 +0200

    runnable 0.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:firstapp/pages/client_home_screen.dart';
-import 'package:firstapp/pages/login_page.dart';
 import 'package:firstapp/pages/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -7,23 +5,14 @@ void main() {
   runApp(const MyApp());
 }
 
-// functions
-void containerTapped() {
-  print("tappedme");
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
-      routes: {
-        '/firstpage': (context) => FirstPage(),
-        '/secondpage': (context) => SecondPage(),
-      },
     );
   }
 }
